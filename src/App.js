85ae495b7fab6bda813c271150807f749db81177
Your GitHub Username <your.email@example.com>
2025-06-23 18:37:10 +0100

    add the pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <h1>AOSMOVIES site web</h1>
+    <main >
+      <Outlet/>
+    </main>
   );
 }
 
---
+++ src/pages/ExplorePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExplorePage = () => {
+  return (
+    <div>
+      explorePage
+    </div>
+  )
+}
+
+export default ExplorePage
