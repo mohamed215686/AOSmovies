aecad413eb83d80b3b23754405b763780c7e2662
Your GitHub Username <your.email@example.com>
2025-06-24 14:56:32 +0100

    header 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,38 @@
 import React from 'react'
-
+import './Header.css'
+import { NavLink } from 'react-router-dom';
 const Header = () => {
+  const navigation=[
+    {
+      label : "TV shows",
+      href : 'tv'
+    },
+    {
+      label : "Movies",
+      href : 'movie'
+    }
+  ]
   return (
-    <div>
-      Header
+    <div className='fixed-header'>
+      <div className='container'>
+        <div>
+          <img src='logo.png' alt='logo' width={120} />
+        </div>
+        <nav className='nav-links'>
+          {
+            navigation.map((nav,index)=>{
+              return(
+                <div >
+                  <NavLink key={nav.label} to={nav.href} className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                    {nav.label}
+                  </NavLink>
+                </div>
+              )})
+          }
+        </nav>
+      </div>
     </div>
+    
   )
 }
 
