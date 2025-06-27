4c4c88ece3b77f2ebd45eef08979a5fc9d1fd80e
YahyaMoaad <yahya.moaad123@gmail.com>
2025-06-27 12:10:32 +0100

    Fix header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,45 +18,47 @@ const Header = () => {
 
   return (
     <div className='fixed-header'>
-      <div className='container '>
-        <div className='logo'>
-          <Link to ={'/'}>
-          <img src='logo.png' alt='logo' width={120} />
-        </Link>
-        </div>
-        
-        <nav className='nav-links top-0'>
-          {
-            navigation.map((nav,index)=>{
-              return(
-                <div >
-                  <NavLink key={nav.label} to={nav.href} className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-                    {nav.label}
-                  </NavLink>
-                </div>
-              )})
-          }
-        </nav>
-        <div className='user '>
-          <form className='search_form' onSubmit={handleSubmit}>
-            <input 
-            type='text'
-            placeholder='search here...'
-            className='search_bar hidden lg:block'
-            onChange={(e)=>SetSearchInput(e.target.value)}
-            value={SearchInput}
-            />
-            <button className='search_icon'>
-                <FaSearch/>
-            </button>
-          </form>
-        
-          <div className="user_icon">
-                <FaUserAlt />
-          </div>
-        </div>
-      </div>
+      <div className='container'>
+  
+  <div className='left-side'>
+    <div className='logo'>
+      <Link to={'/'}>
+        <img src='logo.png' alt='logo' width={120} />
+      </Link>
     </div>
+
+    <nav className='nav-links'>
+      {navigation.map((nav) => (
+        <div key={nav.label}>
+          <NavLink to={nav.href} className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+            {nav.label}
+          </NavLink>
+        </div>
+      ))}
+    </nav>
+  </div>
+
+  
+  <div className='user'>
+    <form className='search_form' onSubmit={handleSubmit}>
+      <input 
+        type='text'
+        placeholder='search here...'
+        className='search_bar hidden lg:block'
+        onChange={(e)=>SetSearchInput(e.target.value)}
+        value={SearchInput}
+      />
+      <button className='search_icon'>
+        <FaSearch />
+      </button>
+    </form>
+
+    <div className="user_icon">
+      <FaUserAlt />
+    </div>
+  </div>
+</div>
+</div>
   )
 }
 
